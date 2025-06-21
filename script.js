0eaa65aa09b672eeb52aaee5898bd013044adbb6
Shyam Paghadal <shyampaghadal@gmail.com>
2025-06-21 21:28:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,14 @@
 function firstNonRepeatedChar(str) {
+  if (!str) return null;
+
   const freq = {};
 
-  // Count character frequency
   for (let char of str) {
     freq[char] = (freq[char] || 0) + 1;
   }
 
-  // Find the first character with frequency 1
   for (let char of str) {
-    if (freq[char] === 1) {
-      return char;
-    }
+    if (freq[char] === 1) return char;
   }
 
   return null;
